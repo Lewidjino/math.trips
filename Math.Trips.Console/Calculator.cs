1063df15b1ddf478407147ffe84673e63364d3c5
Lewidjino <61553721+Lewidjino@users.noreply.github.com>
2023-07-22 17:17:50 +0200

    add exponential trip + basic structure

---
+++ Math.Trips.Console/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace Study.Exponential;
+
+public static class Calculator
+{
+    public static (string, double) UNIT => ("one", 1); 
+
+    public static string Print((string, double) tuple)
+        => $"{tuple.Item1} = {tuple.Item2}";
+    public static (string, double) Times(int times, (string, double) num)
+    {
+        var res = 0D;
+        for (var i = 0; i < times; i++)
+            res += num.Item2;
+
+        return ($"{times}X({num.Item1})", res);
+    }
+
+    public static (string, double) Power(int b, int exponent)
+    {
+        var res = UNIT;
+
+        for (var e = 0; e < exponent; e++)
+            res = Times(b, res);
+
+        res.Item1 = $"{b}^{exponent} = {res.Item1}";
+
+        return res;
+    }
+}
+
---
+++ Math.Trips.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Study.Exponential;
+
+var unit = Calculator.UNIT;
+var zero = Calculator.Times(0, unit);
+var one = Calculator.Times(1, unit);
+var three = Calculator.Times(3, unit);
+
+Console.WriteLine(Calculator.Print(zero));
+Console.WriteLine(Calculator.Print(one));
+Console.WriteLine(Calculator.Print(three));
+Console.WriteLine(Calculator.Print(Calculator.Times(4, three)));
+Console.WriteLine(Calculator.Print(Calculator.Power(3, 0)));
+Console.WriteLine(Calculator.Print(Calculator.Power(3, 1)));
+Console.WriteLine(Calculator.Print(Calculator.Power(3, 2)));
+Console.WriteLine(Calculator.Print(Calculator.Power(3, 3)));
+Console.WriteLine(Calculator.Print(Calculator.Power(0, 3)));
+Console.WriteLine(Calculator.Print(Calculator.Power(0, 0)));
